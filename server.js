8b2587147cb1fc5699e7a6a0d0305d2bef790532
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-09-17 15:04:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('https://lh3.googleusercontent.com/-81nCHljgMKc/VGS374TSQEI/AAAAAAAAABs/dinpzeyg1TkCG-JrvucRIn7hYaBP8t-LQCEw/w139-h140-p/img257.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'img257.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'img257.jpg'));
 });
 
 
