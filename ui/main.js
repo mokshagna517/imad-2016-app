79232eebad20773eac9b7c0f02f72dcdcbc13654
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-09-27 14:27:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,13 @@
 console.log('Loaded');
 
      var img=document.getElementById("move");
+     var marginLeft=0;
+     function moveRight(){
+      marginLeft=marginLeft+5;
+      img.style.marginLeft=marginLeft+'px';
+     }
+     
+     
      img.onclick=function(){
-         img.style.margin-left: "100px";
+         var interval=setInterval(moveRight, 50);
      };
