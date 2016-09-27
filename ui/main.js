313381477e002e369837e5f53b91c5c685c2a965
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-09-27 14:44:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,3 +11,14 @@ console.log('Loaded');
      img.onclick=function(){
          var interval=setInterval(moveRight, 50);
      };
+     var element=document.getElementById('counter');
+     var i=0;
+     element.onclick=function(){
+         
+         
+         var span=document.getElementById('count');
+         
+         span.innerHTML=i++;
+         
+         
+     };
