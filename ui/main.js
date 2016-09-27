40060331b88207dab193ecc4d063dc0336d71cc4
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-09-27 11:51:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+var button=document.getElementById('counter');
+ button.onclick=function(){
+     var request= new XMLHttpRequest();
+     request.onreadystatechange=function(){
+         if(request.reeadystate===200){
+             var counter=request.responseText;
+             var span=document.getElementById('count');
+             span.innerHTML=counter.toString();
+             
+         }
+     };
+     request.pen('GET','http://mokshagna517.imad.hasura-app.io/',true);
+     request.send(null);
+ };
