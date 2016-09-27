f318cd75328bd236d113668ee82ea08661be868a
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-09-27 12:00:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var button=document.getElementById('counter');
  button.onclick=function(){
      var request= new XMLHttpRequest();
      request.onreadystatechange=function(){
-         if(request.reeadystate===200){
+         if(request.readystate===200){
              var counter=request.responseText;
              var span=document.getElementById('count');
              span.innerHTML=counter.toString();
