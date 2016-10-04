002d8268af7b38eb6212795395e9797e05a5d14b
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-10-04 20:53:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,20 @@
 console.log('Loaded');
 
-     var img=document.getElementById("move");
-     var marginLeft=0;
-     function moveRight(){
-      marginLeft=marginLeft+1;
-      img.style.marginLeft=marginLeft+'px';
-     }
      
      
-     img.onclick=function(){
-         var interval=setInterval(moveRight, 50);
-     };
-     var element=document.getElementById('counter');
      
-     element.click=function(){
+     
+     
+     element.onclick=function(){
          
          var request=new XMLHttpRequest();
          
          request.onreadystatechange=function(){
              if(request.readystate===XMLHttpRequest.DONE){
-                 if(request.status==='200'){
-                 var c=request.responseText;
+                 if(request.status===200){
+                 var counter=request.responseText;
                  var span=document.getElementById('count');
-                 span.innerHTML=c.toString();
+                 span.innerHTML=counter.toString();
                  }
              }
              
