1ac27080944bce5e30a1a682230469a6395d9ace
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-10-04 20:09:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,10 +18,12 @@ console.log('Loaded');
          var request=new XMLHttpRequest();
          
          request.onreadystatechange=function(){
-             if(request.readystate===200){
+             if(request.readystate===XMLHttpRequest.DONE){
+                 if(request.status===200){
                  var counter=request.responseText;
                  var span=document.getElementById('count');
                  span.innerHTML=counter.toString();
+                 }
              }
              
              
