91f1a9a931077d26af2712aa62eb5e815789ff6d
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-10-05 10:39:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,10 @@
 console.log('Loaded');
-
-     
-     
-     var element=document.getElementById('counter');
-     
-     
-     element.onclick= function() {
+var element=document.getElementById('counter');
+      element.onclick= function () {
          
          var request=new XMLHttpRequest();
          
-         request.onreadystatechange=function(){
+         request.onreadystatechange=function () {
              if(request.readyState===XMLHttpRequest.DONE){
                  if(request.status===200){
                  var counter=request.responseText;
@@ -24,4 +19,16 @@ console.log('Loaded');
          request.send(null);
          
          
+     };
+     var nameInput=document.getElementById('name');
+     var name=nameInput.value;
+     var submit=document.getElementById('submit_btn');
+     submit.onclick=function () {
+        var names=['name1','name2', 'name3', 'name4'];
+        var list='';
+        for(i=0;i<names.length;i++){
+            list+='<li>'+names[i]+'</li>';
+        }
+        var ul=document.getElementById('namelist');
+        ul.innerHTML=list;
      };
