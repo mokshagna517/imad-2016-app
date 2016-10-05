f8b03f01e8cb5c2ea61e51fcc67cbb36d6c29bdc
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-10-05 10:14:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ console.log('Loaded');
          var request=new XMLHttpRequest();
          
          request.onreadystatechange=function(){
-             if(request.readystate===XMLHttpRequest.DONE){
+             if(request.readyState===XMLHttpRequest.DONE){
                  if(request.status===200){
                  var counter=request.responseText;
                  var span=document.getElementById('count');
