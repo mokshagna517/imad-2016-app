a8578c07383170c1dd406824c27cc05f186b6826
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-10-28 10:51:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,6 +74,16 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var counter=0;
+app.get('/counter', function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
+
+
+
+
 app.get('/:articleName', function(req,res){
    var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
@@ -86,11 +96,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'img257.jpg'));
 });
-var counter=0;
-app.get('/counter', function(req,res){
-    counter=counter+1;
-    res.send(counter.toString());
-});
+
 app.get('/ui/main.js', function(req,res){
     res.sendFile(path.join(__dirname, 'ui','main.js'));
 });
