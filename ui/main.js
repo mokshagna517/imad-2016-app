32f4bc7290ced4f77c295223689540559b8ebfbb
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-10-28 10:04:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,34 +1,3 @@
 console.log('Loaded');
-var element=document.getElementById("counter");
-      element.onclick= function () {
-         
-         var request=new XMLHttpRequest();
-         
-         request.onreadystatechange=function () {
-             if(request.readyState===XMLHttpRequest.DONE){
-                 if(request.status===200){
-                 var i=request.responseText;
-                 var span=document.getElementById("count");
-                 span.innerHTML=i.toString();
-                 }
-             }
-             
-             
-         };
-         request.open('GET', 'http://mokshagna517.imad.hasura-app.io/counter',true);
-         request.send(null);
-         
-         
-     };
-     var nameInput=document.getElementById('name');
-     var name=nameInput.value;
-     var submit=document.getElementById('submit_btn');
-     submit.onclick=function () {
-        var names=['name1','name2', 'name3', 'name4'];
-        var list='';
-        for(i=0;i<names.length;i++){
-            list+='<li>'+names[i]+'</li>';
-        }
-        var ul=document.getElementById('namelist');
-        ul.innerHTML=list;
-     };
+
+  
