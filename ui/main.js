300463845ec0e25cb27706228219d66747356860
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-10-28 11:30:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,20 @@ console.log('Loaded');
 var c=0;
   var element=document.getElementById("counter");
   element.onclick=function(){
+      var request=new XMLHttpRequest();
+      request.onreadystatechange=function(){
+          if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                c=request.responseText;
+                var p=document.getElementById("count");
+                p.innerHTML=c.toString();
+            }  
+              
+              
+          }
+      };    
+          
+  };      
+
       
-      
-      c=c+1;
-      var p=document.getElementById("count");
-      p.innerHTML=c.toString();
-      
-      
-  };
+    
