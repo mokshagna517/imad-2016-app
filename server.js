c9b05853342251c3d0137bebc7939b590d7c5ea2
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-11-06 02:50:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ var pool=new Pool(config);
 app.get('/test-db',function(err,res){
     
     
-    pool.query('SELECT * FROM test',function(err,result){
+    pool.query('SELECT id FROM test',function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }
