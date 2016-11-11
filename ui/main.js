9d10154b5e0a512ce2a66379d5029fd2b99ba467
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-11-11 20:00:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,48 +1,32 @@
-console.log('Loaded');
-var c=0;
-  var element=document.getElementById("counter");
-  element.onclick=function(){
-      var request=new XMLHttpRequest();
-      request.onreadystatechange=function(){
-          if(request.readyState===XMLHttpRequest.DONE){
-            if(request.status===200){
-                c=request.responseText;
-                var p=document.getElementById("count");
-                p.innerHTML=c.toString();
-            }  
-              
-         }
-      };
-      request.open('GET','http://mokshagna517.imad.hasura-app.io/counter',true);
-      request.send(null);
- };      
+   
 
-      var sub=document.getElementById('submit_btn');
-      sub.onclick=function(){
+      var submit=document.getElementById('submit_btn');
+      submit.onclick=function(){
           var request=new XMLHttpRequest();
           request.onreadystatechange=function(){
               if(request.readyState===XMLHttpRequest.DONE){
                   if(request.status===200){
-                      var names=request.responseText;
-                      names=JSON.parse(names);
-                      var list='';
-                      for(var i=0;i<names.length;i++){
-                          list+='<li>'+names[i]+'</li>';
+                      console.log("user registered");
+                      alert("New user registerd successfully");
                       }
-                      var ul=document.getElementById("namelist");
-                      ul.innerHTML=list;
+                      else{
+                          alert("Error ");
+                      }
                       
                       
                   }
                   
-              }
+              
               
           };
           
-var nameInput=document.getElementById("name");
-              var name=nameInput.value;
-              request.open('GET','http://mokshagna517.imad.hasura-app.io/submit-name/?name',true);
-              request.send(null);
+var username=document.getElementById("username").value;
+              var password=document.getElementById("password").value;
+              request.open('POST','http://mokshagna517.imad.hasura-app.io/create-user',true);
+              console.log('username');
+              console.log('password');
+              request.setRequestHeader('Content-Type','application/json');
+              request.send(JSON.stringify({username :username,password:password}));
           
           
       };
