0e829d93435d73cad5dbfe3c3584c665a55cf210
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-11-12 15:29:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,36 +1,6 @@
    
 
-      var submit=document.getElementById("submit_btn");
-      submit.onclick = function() {
-          var request=new XMLHttpRequest();
-          request.onreadystatechange=function(){
-              if(request.readyState===XMLHttpRequest.DONE){
-                  if(request.status===200){
-                      console.log("user registered");
-                      alert("New user registerd successfully");
-                      }
-                      else{
-                          alert("Error ");
-                      }
-                      
-              }  
-          };     
-                  
-              
-              
-        
-          
-var username=document.getElementById("username").value;
-              var password=document.getElementById("password").value;
-               console.log(username);
-              console.log(password);
-              request.open('POST','http://mokshagna517.imad.hasura-app.io/create-user', true);
-             
-              request.setRequestHeader('Content-Type','application/json');
-              request.send(JSON.stringify({username :username,password:password}));
-          
-          
-      };
+     
 
       var sub=document.getElementById("submit-btn");
       sub.onclick=function(){
