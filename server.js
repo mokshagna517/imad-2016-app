231c3bbc144ec643f128f866b8671d12e83a780a
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-11-12 14:12:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,41 @@ app.post('/create-user',function(req,res){
         
     });
 });
+app.post('/login',function(req,res){
+    var username=req.body.username;
+    var password=req.body.password;
+    
+    pool.query('SELECT * FROM "user" WHERE username=$1',[username],function(err,result){
+        
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            if(result.rows.length===0){
+                res.status(403).send("Username/password is invalid");
+            }
+            else{
+                var dbString=result.rows[0].password;
+                var salt=dbString.split('$')[2];
+                var hashedPassword=hash(password,salt);
+                if(hashedPassword===dbString){
+                    res.send("Logged in successfully as"+username);
+                    
+                }
+                else{
+                    res.send("password is incorrect");
+                }
+            }
+        }
+        
+    });
+    
+});  
+    
+    
+    
+   
+    
+
 
 
 
