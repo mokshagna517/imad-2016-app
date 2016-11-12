b2dbef3d9a98df97a6701b0ff3c5367916a269b1
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-11-12 16:09:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@
                           alert("Error ");
                       }
                       
-                      
+                     loadLogin(); 
                   }
                   
               
@@ -33,4 +33,25 @@ var username=document.getElementById("user-name").value;
           
           
       };
+      
+   function loadLoggedInUser (username) {
+    var loginArea = document.getElementById('login_area');
+    loginArea.innerHTML = `
+        <h3> Hi <i>${username}</i></h3>
+        <a href="/logout">Logout</a>
+    `;
+} 
     
+    function loadLogin () {
+    // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                loadLoggedInUser(this.responseText);
+            } 
+        }
+     };
+      request.open('GET', '/check-login', true);
+    request.send(null);
+    }
