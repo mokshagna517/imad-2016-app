849b1c7c8b14e6dfa2f35b7381568ac759cdf8bf
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-11-12 14:12:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,4 +31,36 @@ var username=document.getElementById("username").value;
           
           
       };
+
+      var submit=document.getElementById('submit-btn');
+      submit.onclick=function(){
+          var request=new XMLHttpRequest();
+          request.onreadystatechange=function(){
+              if(request.readyState===XMLHttpRequest.DONE){
+                  if(request.status===200){
+                      console.log("user logged in");
+                      alert("You logged in successfully as");
+                      }
+                      else{
+                          alert("Error ");
+                      }
+                      
+                      
+                  }
+                  
+              
+              
+          };
+          
+var username=document.getElementById("user-name").value;
+              var password=document.getElementById("pass-word").value;
+               console.log(username);
+              console.log(password);
+              request.open('POST','http://mokshagna517.imad.hasura-app.io/login', true);
+             
+              request.setRequestHeader('Content-Type','application/json');
+              request.send(JSON.stringify({username :username,password:password}));
+          
+          
+      };
     
