92bd00f83785b24d999998e0dde27780fab37130
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-11-12 12:10:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,9 +22,9 @@
           
 var username=document.getElementById("username").value;
               var password=document.getElementById("password").value;
-               console.log('username');
-              console.log('password');
-              request.open('POST','http://mokshagna517.imad.hasura-app.io/create-user',true);
+               console.log(username);
+              console.log(password);
+              request.open('POST','http://mokshagna517.imad.hasura-app.io/create-user', true);
              
               request.setRequestHeader('Content-Type','application/json');
               request.send(JSON.stringify({username :username,password:password}));
