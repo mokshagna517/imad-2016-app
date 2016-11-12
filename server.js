2d5537a1229a9ad0d559d1e54086bf547f7b321e
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-11-12 14:40:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool=require('pg').Pool;
 var crypto=require('crypto');
 var bodyParser=require('body-parser');
+var session=require('express-session');
 var config={
     user: 'mokshagna517',
     db: 'mokshagna517',
@@ -16,6 +17,10 @@ var config={
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+   secret: "Somerandomstring",
+   cookie: {maxAge: 1000*60*60*24*30}
+}));
 var pool=new Pool(config);
 function createTemplate( data){
     var tit=data.title;
@@ -105,6 +110,7 @@ app.post('/login',function(req,res){
                 var salt=dbString.split('$')[2];
                 var hashedPassword=hash(password,salt);
                 if(hashedPassword===dbString){
+                    req.session.auth={userId: result.rows[0].id};
                     res.send("Logged in successfully as"+username);
                     
                 }
@@ -118,9 +124,21 @@ app.post('/login',function(req,res){
     
 });  
     
+ app.get('/check-login',function(req,res){
+     
+     if(req.session&&req.session.auth&&req.session.auth.userId){
+     res.send("You are logged in"+req.session.auth.userId.toString());
+     }
+     else{
+         res.send("You are not logged in");
+     }
+     
+ });   
     
-    
-   
+  app.get('/logout', function(req,res){
+     delete req.session.auth;
+     res.send("You are logged out");
+  }); 
     
 
 
