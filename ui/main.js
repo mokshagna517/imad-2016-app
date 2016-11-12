aca1cd192f5fbae3ce0a655f93c6c1fccc2a6226
mokshagna517 <mokshagnasaiteja517@gmail.com>
2016-11-12 16:21:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,13 +36,7 @@ var username=document.getElementById("user-name").value;
       
   
  
-     function loadLoggedInUser (username) {
-    var loginArea = document.getElementById('login_area');
-    loginArea.innerHTML = `
-        <h3> Hi <i>${username}</i></h3>
-        <a href="/logout">Logout</a>
-    `;
-     }
+   
     function loadLogin () {
     // Check if the user is already logged in
     var request = new XMLHttpRequest();
